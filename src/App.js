21c0ad7e4bc79789d04f905e6059880c68b5ff46
Frankie Riviera <frankgriviera@outlook.com>
2025-04-03 16:32:01 -0400

    Updated video

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="video-container">
         <div className="iframe-wrapper">
         <video controls className="video" width="100%" height="auto">
-        <source src="/videos/AzureOpenAIIn1Minute.mp4" type="video/mp4" />
+        <source src="https://blobvideohostsa.blob.core.windows.net/videocontainer/WhyAzure.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
         </div>

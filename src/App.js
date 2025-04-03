b21fb410c79eff769b018402d5222afb000a79fa
Frankie Riviera <frankgriviera@outlook.com>
2025-04-03 15:30:39 -0400

    Added small video file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
       </header>
       <div className="video-container">
         <div className="iframe-wrapper">
-        <iframe type="text/html" frameborder="0" width="1920" height="1080" src="https://www.youtube.com/embed/tvyTtYWaumY" allowfullscreen></iframe>
+        <video controls className="video" width="100%" height="auto">
+        <source src="/videos/AzureOpenAIIn1Minute.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
         </div>
       </div>
     </div>
